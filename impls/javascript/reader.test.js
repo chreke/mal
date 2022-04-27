bbdc4a2a03c3cff61c4165e6dcdcc29af3c4a4bd
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-27 23:56:11 +0200

    Add initial version of reader

---
+++ impls/javascript/reader.test.js
@@ -0,0 +1,17 @@
+const { tokenize } = require("./reader");
+
+const tokenizeTestCases = [
+  [`10`, [`10`]],
+  [`-10`, [`-10`]],
+  [`foo`, [`foo`]],
+  [`:foo`, [`:foo`]],
+  [`"foo \\"bar"`, [`"foo \\"bar"`]],
+  [`"foo \\nbar"`, [`"foo \\nbar"`]],
+  [`"foo bar"`, ['"foo bar"']],
+  ["(+ 4 10)", ["(", "+", "4", "10", ")"]],
+  ["'(4 10)", ["'", "(", "4", "10", ")"]],
+];
+
+test.each(tokenizeTestCases)("should tokenize %s", (string, tokens) => {
+  expect(tokenize(string)).toEqual(tokens);
+});
