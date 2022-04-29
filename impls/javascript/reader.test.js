44774d527af682631fbf4a256c7e9feb39ca1148
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-30 00:08:58 +0200

    Reorganize tokenize test

--- impls/javascript/reader.test.js
+++ impls/javascript/reader.test.js
@@ -1,18 +1,19 @@
 const { tokenize } = require("./reader");
 
-const tokenizeTestCases = [
-  [`10`, [`10`]],
-  [`-10`, [`-10`]],
-  [`foo`, [`foo`]],
-  [`:foo`, [`:foo`]],
-  [`"foo \\"bar"`, [`"foo \\"bar"`]],
-  [`"foo \\nbar"`, [`"foo \\nbar"`]],
-  [`"foo bar"`, ['"foo bar"']],
-  ["(+ 4 10)", ["(", "+", "4", "10", ")"]],
-  ["'(4 10)", ["'", "(", "4", "10", ")"]],
-  [" ( 4, 10 5 ) ", ["(", "4", "10", "5", ")"]],
-];
-
-test.each(tokenizeTestCases)("should tokenize %s", (string, tokens) => {
-  expect(tokenize(string)).toEqual(tokens);
+describe("tokenize", () => {
+  const tokenizeTestCases = [
+    [`10`, [`10`]],
+    [`-10`, [`-10`]],
+    [`foo`, [`foo`]],
+    [`:foo`, [`:foo`]],
+    [`"foo \\"bar"`, [`"foo \\"bar"`]],
+    [`"foo \\nbar"`, [`"foo \\nbar"`]],
+    [`"foo bar"`, ['"foo bar"']],
+    ["(+ 4 10)", ["(", "+", "4", "10", ")"]],
+    ["'(4 10)", ["'", "(", "4", "10", ")"]],
+    [" ( 4, 10 5 ) ", ["(", "4", "10", "5", ")"]],
+  ];
+  test.each(tokenizeTestCases)("should tokenize %s", (string, tokens) => {
+    expect(tokenize(string)).toEqual(tokens);
+  });
 });
