9929bd2d5170f66d56ec5035832c63ca60388a2e
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-04-30 00:34:50 +0200

    Add readForm function

--- impls/javascript/reader.js
+++ impls/javascript/reader.js
@@ -1,4 +1,24 @@
-const tokenize = (input) => {
+class Quote {
+  constructor(expression) {
+    this.expression = expression;
+  }
+
+  toString() {
+    throw Error("Not implemented");
+  }
+}
+
+class Sym {
+  constructor(name) {
+    this.name = name;
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+function tokenize(input) {
   const unqouteSplice = "~@";
   const specialChars = /[\[\]{}()'`~^@]/;
   const string = /"(?:\\.|[^\\"])*"?/;
@@ -24,6 +44,49 @@ const tokenize = (input) => {
     matches.push(match[0]);
   }
   return matches;
-};
+}
 
-module.exports = { tokenize };
+function readForm(tokens) {
+  const token = tokens.shift();
+  if (token === "(") {
+    return readList(tokens);
+  } else if (token === "'") {
+    return readQuote(tokens);
+  }
+  return readAtom(token);
+}
+
+function readList(tokens) {
+  const items = [];
+  while (tokens.length) {
+    if (tokens[0] === ")") {
+      tokens.shift();
+      return items;
+    }
+    items.push(readForm(tokens));
+  }
+  throw SyntaxError("Unmatched parentheses");
+}
+
+function readQuote(tokens) {
+  return new Quote(readForm(tokens));
+}
+
+function readAtom(token) {
+  if (token.match(/^[-+]?[0-9]+$/)) {
+    return parseInt(token);
+  } else if (token.match(/^[-+]?[0-9]+\.[0-9]*$/)) {
+    return parseFloat(token);
+  } else if (token === "nil") {
+    return null;
+  } else if (token === /true|false/) {
+    return token === "true";
+  } else if (token[0] === `"`) {
+    // TODO: Parse escape sequences etc
+    return token;
+  } else {
+    return new Sym(token);
+  }
+}
+
+module.exports = { readForm, tokenize, Sym, Quote };
--- impls/javascript/reader.test.js
+++ impls/javascript/reader.test.js
@@ -1,4 +1,4 @@
-const { tokenize } = require("./reader");
+const { tokenize, readForm, Sym, Quote } = require("./reader");
 
 describe("tokenize", () => {
   const tokenizeTestCases = [
@@ -15,5 +15,20 @@ describe("tokenize", () => {
   ];
   test.each(tokenizeTestCases)("should tokenize %s", (string, tokens) => {
     expect(tokenize(string)).toEqual(tokens);
+  });
+});
+
+describe("readForm", () => {
+  const readerTestCases = [
+    ["3", 3],
+    ["nil", null],
+    ["(+ 5 6 7)", [new Sym("+"), 5, 6, 7]],
+    ["(/ (+ 5 -4) 6)", [new Sym("/"), [new Sym("+"), 5, -4], 6]],
+    ["'foo", new Quote(new Sym("foo"))],
+    ["'(+ 1 2)", new Quote([new Sym("+"), 1, 2])],
+    ["'(1 2 3)", new Quote([1, 2, 3])],
+  ];
+  test.each(readerTestCases)("should read %s", (program, forms) => {
+    expect(readForm(tokenize(program))).toEqual(forms);
   });
 });
