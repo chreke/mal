b912444030df01b8d4de798238b1ccb5ab07681b
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2022-05-20 14:27:37 +0200

    Update REPL

--- impls/javascript/repl.js
+++ impls/javascript/repl.js
@@ -1,16 +1,19 @@
 const readline = require("readline");
-const { stdin, stdout } = require("process")
-const rl = readline.createInterface({input: stdin, output: stdout});
+const { stdin, stdout } = require("process");
+const { readStr } = require("./reader");
+const { printStr } = require("./printer");
 
-const read = (x) => x;
+const rl = readline.createInterface({ input: stdin, output: stdout });
+
+const read = (x) => readStr(x);
 
 const eval = (x) => x;
 
-const print = (x) => x;
+const print = (x) => printStr(x);
 
 const rep = (input) => {
   console.log(print(eval(read(input))));
-}
+};
 
 rl.setPrompt("user> ");
 rl.prompt();
